75b2a668dd1f3349dacb35ff7e8a103933586766
svenvandevelde <sven.van.de.velde@telenet.be>
2016-05-16 09:06:02 +0200

    Updated documentation for missile trainer

--- Test Missions/Moose_Test_MISSILETRAINER/Moose_Test_MISSILETRAINER.lua
+++ Test Missions/Moose_Test_MISSILETRAINER/Moose_Test_MISSILETRAINER.lua
@@ -1,6 +1,19 @@
 
-
-Include.File("Moose")
+-- Only use Include.File when developing new MOOSE classes.
+-- When using Moose.lua in the DO SCIPTS FILE initialization box, 
+-- these Include.File statements are not needed, because all classes within Moose will be loaded.
 Include.File("MissileTrainer")
 
-local Trainer = MISSILETRAINER:New( 200 )
+-- This is an example of a global
+local Trainer = MISSILETRAINER
+  :New( 200 )
+  :InitMessagesOnOff(true)
+  :InitAlertsToAll(false) -- I'll correct it below ...
+  :InitAlertsHitsOnOff(true)
+  :InitAlertsLaunchesOnOff(true)
+  :InitBearingOnOff(true)
+  :InitRangeOnOff(true)
+  :InitTrackingOnOff(true)
+  :InitTrackingToAll(true)
+
+Trainer:InitAlertsHitsOnOff(true) -- Now alerts are also on
