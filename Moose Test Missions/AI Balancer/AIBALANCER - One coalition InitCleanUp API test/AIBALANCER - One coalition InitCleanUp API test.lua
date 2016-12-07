0d8936bcb0fdd6fda5d48c7f9ab3f47c7d9b244f
FlightControl <sven.van.de.velde@telenet.be>
2016-12-07 14:55:35 +0100

    Test Case documentation

--- Moose Test Missions/AI Balancer/AIBALANCER - One coalition InitCleanUp API test/AIBALANCER - One coalition InitCleanUp API test.lua
+++
@@ -1,4 +0,0 @@
-
-local RU_PlanesClientSet = SET_CLIENT:New():FilterCountries( "RUSSIA" ):FilterCategories( "plane" ):FilterStart()
-local RU_PlanesSpawn = SPAWN:New( "AI RU" ):InitCleanUp( 20 )
-local RU_AIBalancer = AIBALANCER:New( RU_PlanesClientSet, RU_PlanesSpawn )
