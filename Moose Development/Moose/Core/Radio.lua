ad0cac8356c4c5d36ed5c82a884fc7643e06366f
Grey-Echo <hugues.bousquet@outlook.fr>
2017-03-10 23:31:32 +0100

    Laying the groundwork for the RADIO class
    With a special emphasis on LDT Documentation

---
+++ Moose Development/Moose/Core/Radio.lua
@@ -0,0 +1,15 @@
+--- This module contains the MESSAGE class.
+-- 
+-- 1) @{Radio#RADIO} class, extends @{Base#BASE}
+-- =================================================
+-- Radio system to manage radio communications
+-- Radio transmissions consist of sound files that are broadcasted on a specific channel and modulation 
+-- If sent by a UNIT or a GROUP, Radio communications can be subtitled for a specific amount of time
+-- 
+-- 1.1) RADIO construction methods
+-- -------------------------------
+-- RADIO is created with @{Radio#RADIO.New}. This doesn't broadcast a transmission, but only create a RADIO object
+-- To actually broadcast your transmission, you need to use @{Radio#RADIO.Broadcast}
+--   
+-- @module Radio
+-- @author Grey-Echo
