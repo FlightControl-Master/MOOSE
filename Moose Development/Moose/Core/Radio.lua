c37d6e6f95abde2fba515b2d29ecf5a198890d17
Grey-Echo <hugues.bousquet@outlook.fr>
2017-03-10 23:31:53 +0100

    Implements RADIO's constructor

--- Moose Development/Moose/Core/Radio.lua
+++ Moose Development/Moose/Core/Radio.lua
@@ -9,6 +9,7 @@
 -- 1.1) RADIO construction methods
 -- -------------------------------
 -- RADIO is created with @{Radio#RADIO.New}. This doesn't broadcast a transmission, but only create a RADIO object
+-- It should only be used internally. To create a RADIO object, please use @{Identifiable#IDENIFIABLE.GetRadio}
 -- To actually broadcast your transmission, you need to use @{Radio#RADIO.Broadcast}
 --   
 -- @module Radio
@@ -27,4 +28,23 @@ MESSAGE = {
     Subtitle = "",
     SubtitleDuration = ""
 }
+
+--- Create a new RADIO Object. This doesn't broadcast a transmission, though, use @{Radio#RADIO.Broadcast} to actually broadcast
+-- @param self
+-- @param 
+-- @return #RADIO
+-- @usage
+-- -- This is for internal use only. To create a new RADIO, use @{Identifiable#IDENIFIABLE.GetRadio}
+function RADIO:New(identifiable)
+    local self = BASE:Inherit( self, BASE:New() )
+    self:F( { MessageText, MessageDuration, MessageCategory } )
+    
+    self.Identifiable = identifiable
+    return self
+end
+
+
+    
+
+
 
