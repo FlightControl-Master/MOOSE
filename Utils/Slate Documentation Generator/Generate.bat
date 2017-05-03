be983ccbd58237a44cb1f040ddf16b7d5a098fd9
Grey-Echo <hugues.bousquet@outlook.fr>
2017-05-03 16:58:31 +0200

    Doc

--- Utils/Slate Documentation Generator/Generate.bat
+++ Utils/Slate Documentation Generator/Generate.bat
@@ -8,3 +8,7 @@ rmdir /s /q "./bin/TEMP"
 
 :: Copy the Images that go with the documentation
 robocopy ../../docs/Presentations ../../../slate/source/includes/Pictures /MIR /NJH /NJS
+
+:: Deploy the Slate documentation
+"C:\Program Files\Git\bin\bash.exe" --login -i -c "cd slate | git commit -a -m "Doc Update" | ./deploy.sh"
+
