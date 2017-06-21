54c8a6f9ddd17d8fcac5c6303ae99556eb0f4a17
FlightControl <sven.van.de.velde@telenet.be>
2017-06-21 06:00:24 +0200

    Updates

--- Moose Mission Setup/Moose.lua
+++ Moose Mission Setup/Moose.lua
@@ -1,5 +1,5 @@
 env.info( '*** MOOSE DYNAMIC INCLUDE START *** ' )
-env.info( 'Moose Generation Timestamp: 20170612_0516' )
+env.info( 'Moose Generation Timestamp: 20170620_2133' )
 
 local base = _G
 
