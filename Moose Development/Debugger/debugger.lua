e229e2e381cf39d50bbaa6ddf6eda45816dd9422
FlightControl_Master <sven.van.de.velde@telenet.be>
2017-11-07 18:06:18 +0100

    Documentation

--- Moose Development/Debugger/debugger.lua
+++ Moose Development/Debugger/debugger.lua
@@ -1,4 +1,7 @@
 -------------------------------------------------------------------------------
+-- MOOSE VERSION for DCS WORLD
+-- Modified by FlightControl to understand "DCS".
+-------------------------------------------------------------------------------
 -- Copyright (c) 2011-2012 Sierra Wireless and others.
 -- All rights reserved. This program and the accompanying materials
 -- are made available under the terms of the Eclipse Public License v1.0
