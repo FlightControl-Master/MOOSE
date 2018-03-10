3544f071696eb4ede722c252cef5e73ff0940292
FlightControl_Master <sven.van.de.velde@telenet.be>
2018-03-10 08:44:33 +0100

    If one or more units of a group defined as CARGO_GROUP died, the CARGO_GROUP:Board() command does not trigger the CARGO_GRUOP:OnEnterLoaded() function.

--- Moose Development/Moose/Core/Cargo.lua
+++ Moose Development/Moose/Core/Cargo.lua
@@ -1289,7 +1289,10 @@ function CARGO_GROUP:onafterBoarding( From, Event, To, CargoCarrier, NearRadius,
   -- For each Cargo object within the CARGO_GROUP, route each object to the CargoLoadPointVec2
   for CargoID, Cargo in pairs( self.CargoSet:GetSet() ) do
     self:T( { Cargo:GetName(), Cargo.current } )
-    if not Cargo:is( "Loaded" ) then
+    
+    
+    if not Cargo:is( "Loaded" ) 
+    and (not Cargo:is( "Destroyed" )) then -- If one or more units of a group defined as CARGO_GROUP died, the CARGO_GROUP:Board() command does not trigger the CARGO_GRUOP:OnEnterLoaded() function.
       Boarded = false
     end
     
