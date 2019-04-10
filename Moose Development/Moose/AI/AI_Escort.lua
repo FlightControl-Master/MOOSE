03c47cc9f9c9efdea68c79b26d26de8cd547236b
FlightControl <FlightControl_Moose@outlook.com>
2019-04-10 18:49:05 +0200

    documentation

--- Moose Development/Moose/AI/AI_Escort.lua
+++ Moose Development/Moose/AI/AI_Escort.lua
@@ -25,11 +25,49 @@
 -- 
 -- Allows you to interact with escorting AI on your flight and take the lead.
 -- 
--- Each escorting group can be commanded with a whole set of radio commands (radio menu in your flight, and then F10).
+-- Each escorting group can be commanded with a complete set of radio commands (radio menu in your flight, and then F10).
 --
--- The radio commands will vary according the category of the group. The richest set of commands are with Helicopters and AirPlanes.
+-- The radio commands will vary according the category of the group. The richest set of commands are with helicopters and airPlanes.
 -- Ships and Ground troops will have a more limited set, but they can provide support through the bombing of targets designated by the other escorts.
---
+-- 
+-- Escorts detect targets using a built-in detection mechanism. The detected targets are reported at a specified time interval.
+-- Once targets are reported, each escort has these targets as menu options to command the attack of these targets.
+-- Targets are by default grouped per area of 5000 meters, but the kind of detection and the grouping range can be altered.
+-- 
+-- Different formations can be selected in the Flight menu: Trail, Stack, Left Line, Right Line, Left Wing, Right Wing, Central Wing and Boxed formations are available.
+-- The Flight menu also allows for a mass attack, where all of the escorts are commanded to attack a target.
+-- 
+-- Escorts can emit flares to reports their location. They can be commanded to hold at a location, which can be their current or the leader location.
+-- In this way, you can spread out the escorts over the battle field before a coordinated attack.
+-- 
+-- But basically, the escort class provides 4 modes of operation, and depending on the mode, you are either leading the flight, or following the flight.
+-- 
+-- ## Leading the flight
+-- 
+-- When leading the flight, you are expected to guide the escorts towards the target areas, 
+-- and carefully coordinate the attack based on the threat levels reported, and the available weapons
+-- carried by the escorts. Ground ships or ground troops can execute A-assisted attacks, when they have long-range ground precision weapons for attack.
+-- 
+-- ## Following the flight
+-- 
+-- Escorts can be commanded to execute a specific mission path. In this mode, the escorts are in the lead.
+-- You as a player, are following the escorts, and are commanding them to progress the mission while
+-- ensuring that the escorts survive. You are joining the escorts in the battlefield. They will detect and report targets
+-- and you will ensure that the attacks are well coordinated, assigning the correct escort type for the detected target
+-- type. Once the attack is finished, the escort will resume the mission it was assigned.
+-- In other words, you can use the escorts for reconnaissance, and for guiding the attack.
+-- Imagine you as a mi-8 pilot, assigned to pickup cargo. Two ka-50s are guiding the way, and you are
+-- following. You are in control. The ka-50s detect targets, report them, and you command how the attack
+-- will commence and from where. You can control where the escorts are holding position and which targets
+-- are attacked first. You are in control how the ka-50s will follow their mission path.
+-- 
+-- Escorts can act as part of a AI A2G dispatcher offensive. In this way, You was a player are in control.
+-- The mission is defined by the A2G dispatcher, and you are responsible to join the flight and ensure that the
+-- attack is well coordinated.
+-- 
+-- It is with great proud that I present you this class, and I hope you will enjoy the functionality and the dynamism
+-- it brings in your DCS world simulations.
+-- 
 -- # RADIO MENUs that can be created:
 -- 
 -- Find a summary below of the current available commands:
@@ -38,7 +76,7 @@
 -- 
 -- Escort group navigation functions:
 --
---   * **"Join-Up and Follow at x meters":** The escort group fill follow you at about x meters, and they will follow you.
+--   * **"Join-Up":** The escort group fill follow you in the assigned formation.
 --   * **"Flare":** Provides menu commands to let the escort group shoot a flare in the air in a color.
 --   * **"Smoke":** Provides menu commands to let the escort group smoke the air in a color. Note that smoking is only available for ground and naval troops.
 --
@@ -46,32 +84,33 @@
 -- 
 -- Escort group navigation functions:
 --
---   * **"At current location":** Stops the escort group and they will hover 30 meters above the ground at the position they stopped.
---   * **"At client location":** Stops the escort group and they will hover 30 meters above the ground at the position they stopped.
+--   * **"At current location":** The escort group will hover above the ground at the position they were. The altitude can be specified as a parameter.
+--   * **"At my location":** The escort group will hover or orbit at the position where you are. The escort will fly to your location and hold position. The altitude can be specified as a parameter.
 --
 -- ## Report targets ...:
 -- 
--- Report targets will make the escort group to report any target that it identifies within a 8km range. Any detected target can be attacked using the 4. Attack nearby targets function. (see below).
+-- Report targets will make the escort group to report any target that it identifies within detection range. Any detected target can be attacked using the "Attack Targets" menu function. (see below).
 --
 --   * **"Report now":** Will report the current detected targets.
---   * **"Report targets on":** Will make the escort group to report detected targets and will fill the "Attack nearby targets" menu list.
+--   * **"Report targets on":** Will make the escorts to report the detected targets and will fill the "Attack Targets" menu list.
 --   * **"Report targets off":** Will stop detecting targets.
---
--- ## Scan targets ...:
--- 
--- Menu items to pop-up the escort group for target scanning. After scanning, the escort group will resume with the mission or defined task.
---
---   * **"Scan targets 30 seconds":** Scan 30 seconds for targets.
---   * **"Scan targets 60 seconds":** Scan 60 seconds for targets.
 --
 -- ## Attack targets ...:
 -- 
 -- This menu item will list all detected targets within a 15km range. Depending on the level of detection (known/unknown) and visuality, the targets type will also be listed.
+-- This menu will be available in Flight menu or in each Escort menu.
+--
+-- ## Scan targets ...:
+-- 
+-- Menu items to pop-up the escort group for target scanning. After scanning, the escort group will resume with the mission or rejoin formation.
+--
+--   * **"Scan targets 30 seconds":** Scan 30 seconds for targets.
+--   * **"Scan targets 60 seconds":** Scan 60 seconds for targets.
 --
 -- ## Request assistance from ...:
 -- 
--- This menu item will list all detected targets within a 15km range, as with the menu item **Attack Targets**.
--- This menu item allows to request attack support from other escorts supporting the current client group.
+-- This menu item will list all detected targets within a 15km range, similar as with the menu item **Attack Targets**.
+-- This menu item allows to request attack support from other ground based escorts supporting the current escort.
 -- eg. the function allows a player to request support from the Ship escort to attack a target identified by the Plane escort with its Tomahawk missiles.
 -- eg. the function allows a player to request support from other Planes escorting to bomb the unit with illumination missiles or bombs, so that the main plane escort can attack the area.
 --
@@ -111,22 +150,12 @@
 
 --- @type AI_ESCORT
 -- @extends AI.AI_Formation#AI_FORMATION
--- @field Wrapper.Client#CLIENT EscortUnit
--- @field Wrapper.Group#GROUP EscortGroup
--- @field #string EscortName
--- @field #AI_ESCORT.MODE EscortMode The mode the escort is in.
--- @field Core.Scheduler#SCHEDULER FollowScheduler The instance of the SCHEDULER class.
--- @field #number FollowDistance The current follow distance.
--- @field #boolean ReportTargets If true, nearby targets are reported.
--- @Field DCS#AI.Option.Air.val.ROE OptionROE Which ROE is set to the EscortGroup.
--- @field DCS#AI.Option.Air.val.REACTION_ON_THREAT OptionReactionOnThreat Which REACTION_ON_THREAT is set to the EscortGroup.
--- @field FunctionalMENU_GROUPDETECTION_BASE Detection
 
 --- AI_ESCORT class
 -- 
 -- # AI_ESCORT construction methods.
 -- 
--- Create a new SPAWN object with the @{#AI_ESCORT.New} method:
+-- Create a new AI_ESCORT object with the @{#AI_ESCORT.New} method:
 --
 --  * @{#AI_ESCORT.New}: Creates a new AI_ESCORT object from a @{Wrapper.Group#GROUP} for a @{Wrapper.Client#CLIENT}, with an optional briefing text.
 --
