2738f1ef6aa3008eceadfd033c1f25254c9baedc
OttoWerkr <31276014+OttoWerkr@users.noreply.github.com>
2021-01-14 09:57:11 -0500

    Update Scenery.lua
    Adds functional FindByName() to SCENERY class

--- Moose Development/Moose/Wrapper/Scenery.lua
+++ Moose Development/Moose/Wrapper/Scenery.lua
@@ -57,3 +57,57 @@ end
 function SCENERY:GetThreatLevel()
   return 0, "Scenery"
 end
+
+
+function SCENERY:FindByName(id)
+  local sceneryObject = nil
+--if this is the first time thru this function, locate a land based airbase, and scan it for a valid scenery object
+  if SCENERY.SceneryObject == nil then
+-- iterate thru the list of 'airbases' to find a land based airbase.  Sea based airbases will not work. 
+-- return the first land based airbase
+    local getLandBase = function(airbases)
+      for index,base in ipairs(airbases) do
+        local pt = base:getPoint()
+        local landtype = land.getSurfaceType({x = pt.x, y=pt.z})
+        if landtype ~= land.SurfaceType.SHALLOW_WATER and landtype ~= land.SurfaceType.WATER then
+          return base
+        end
+      end
+    end
+-- retrieve a list of airbases on the current map/mission 
+    local base = world.getAirbases()
+-- call function getLandBase() to get the first land based airbase, and save it's coords.
+    local basecoord = getLandBase(base):getPoint()
+    local foundUnits = {}
+-- set up a 400m sphere around the coord of the found airbase to scan for scenery
+    local searchsphere = 
+    {
+      id = world.VolumeType.SPHERE,
+      params = 
+      {
+        point = basecoord,
+        radius = 200,
+      }
+    }
+-- function to add a scenery object to the list of 'foundUnits'
+    local ifFound = function(foundItem, val)
+      foundUnits[#foundUnits + 1] = foundItem
+      return true
+    end
+-- search in the sphere 'searchsphere' for a SCENERY type object.
+-- if a SCENERY object is found, call function ifFound() to add it to list.    
+    local tempobjects = world.searchObjects(Object.Category.SCENERY, searchsphere, ifFound)
+-- if any scenery was found in the sphere, add the first found object to the SCENERY template (SCENERY.SceneryObject    
+    if tempobjects > 0 then
+      sceneryObject = foundUnits[1]
+      SCENERY.SceneryObject = foundUnits[1]
+    end
+  end
+  if SCENERY.SceneryObject then
+-- change the id of the found scenery object, register it, return it.
+    SCENERY.SceneryObject.id_ = id
+    local sceneryobject = SCENERY:Register(SCENERY.SceneryObject.id_,SCENERY.SceneryObject)
+    return sceneryobject
+  end
+  return nil
+end
