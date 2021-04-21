f4f12f786cfb7afd9594a33c5d9eb1c2f1c2c077
Applevangelist <72444570+Applevangelist@users.noreply.github.com>
2021-04-21 18:52:44 +0200

    Update Airboss.lua
    Escape error

--- Moose Development/Moose/Ops/Airboss.lua
+++ Moose Development/Moose/Ops/Airboss.lua
@@ -17846,7 +17846,7 @@ function AIRBOSS:onbeforeLoad(From, Event, To, path, filename)
 
   -- Check default path.
   if path==nil and not lfs then
-    self:E(self.lid.."WARNING: lfs not desanitized. Results will be saved in DCS installation root directory rather than your \"Saved Games\DCS\" folder.")
+    self:E(self.lid.."WARNING: lfs not desanitized. Results will be saved in DCS installation root directory rather than your \"Saved Games\\DCS\" folder.")
   end
 
   -- Set path or default.
