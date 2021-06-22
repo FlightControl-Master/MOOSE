ee503a378ee711e2009858db73559d50c833e321
Applevangelist <72444570+Applevangelist@users.noreply.github.com>
2021-06-22 18:24:24 +0200

    Update Modules.lua (#1557)
    added CTLD

--- Moose Development/Moose/Modules.lua
+++ Moose Development/Moose/Modules.lua
@@ -85,6 +85,7 @@ __Moose.Include( 'Scripts/Moose/Ops/Squadron.lua' )
 __Moose.Include( 'Scripts/Moose/Ops/AirWing.lua' )
 __Moose.Include( 'Scripts/Moose/Ops/Intelligence.lua' )
 __Moose.Include( 'Scripts/Moose/Ops/CSAR.lua' )
+__Moose.Include( 'Scripts/Moose/Ops/CTLD.lua' )
 
 __Moose.Include( 'Scripts/Moose/AI/AI_Balancer.lua' )
 __Moose.Include( 'Scripts/Moose/AI/AI_Air.lua' )
