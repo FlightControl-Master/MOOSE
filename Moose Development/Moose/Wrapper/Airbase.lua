353d6dfec0ed1d574945c537ca6c3e7b87e9f3ea
Frank <frank@inter-zone.de>
2021-07-02 08:51:51 +0200

    Update Airbase.lua
    - Corrected Mariana Islands airbase names
    - Fixed bug in Syria airbase name `["Thalah"]="Tha'lah"`

--- Moose Development/Moose/Wrapper/Airbase.lua
+++ Moose Development/Moose/Wrapper/Airbase.lua
@@ -73,7 +73,7 @@ AIRBASE = {
 
 --- Enumeration to identify the airbases in the Caucasus region.
 --
--- These are all airbases of Caucasus:
+-- Airbases of the Caucasus map:
 --
 --   * AIRBASE.Caucasus.Gelendzhik
 --   * AIRBASE.Caucasus.Krasnodar_Pashkovsky
@@ -122,7 +122,7 @@ AIRBASE.Caucasus = {
   ["Beslan"] = "Beslan",
   }
 
---- These are all airbases of Nevada:
+--- Airbases of the Nevada map:
 --
 --   * AIRBASE.Nevada.Creech_AFB
 --   * AIRBASE.Nevada.Groom_Lake_AFB
@@ -141,6 +141,7 @@ AIRBASE.Caucasus = {
 --   * AIRBASE.Nevada.Pahute_Mesa_Airstrip
 --   * AIRBASE.Nevada.Tonopah_Airport
 --   * AIRBASE.Nevada.Tonopah_Test_Range_Airfield
+--   
 -- @field Nevada
 AIRBASE.Nevada = {
   ["Creech_AFB"] = "Creech AFB",
@@ -162,7 +163,7 @@ AIRBASE.Nevada = {
   ["Tonopah_Test_Range_Airfield"] = "Tonopah Test Range Airfield",
   }
 
---- These are all airbases of Normandy:
+--- Airbases of the Normandy map:
 --
 --   * AIRBASE.Normandy.Saint_Pierre_du_Mont
 --   * AIRBASE.Normandy.Lignerolles
@@ -195,6 +196,7 @@ AIRBASE.Nevada = {
 --   * AIRBASE.Normandy.Funtington
 --   * AIRBASE.Normandy.Tangmere
 --   * AIRBASE.Normandy.Ford_AF
+--   
 -- @field Normandy
 AIRBASE.Normandy = {
   ["Saint_Pierre_du_Mont"] = "Saint Pierre du Mont",
@@ -237,7 +239,7 @@ AIRBASE.Normandy = {
   ["Conches"] = "Conches",
 }
 
---- These are all airbases of the Persion Gulf Map:
+--- Airbases of the Persion Gulf Map:
 --
 -- * AIRBASE.PersianGulf.Abu_Dhabi_International_Airport
 -- * AIRBASE.PersianGulf.Abu_Musa_Island_Airport
@@ -268,6 +270,7 @@ AIRBASE.Normandy = {
 -- * AIRBASE.PersianGulf.Sirri_Island
 -- * AIRBASE.PersianGulf.Tunb_Island_AFB
 -- * AIRBASE.PersianGulf.Tunb_Kochak
+-- 
 -- @field PersianGulf
 AIRBASE.PersianGulf = {
   ["Abu_Dhabi_International_Airport"] = "Abu Dhabi Intl",
@@ -301,7 +304,7 @@ AIRBASE.PersianGulf = {
   ["Tunb_Kochak"] = "Tunb Kochak",
 }
 
---- These are all airbases of the The Channel Map:
+--- Airbases of The Channel Map:
 --
 -- * AIRBASE.TheChannel.Abbeville_Drucat
 -- * AIRBASE.TheChannel.Merville_Calonne
@@ -326,7 +329,7 @@ AIRBASE.TheChannel = {
   ["High_Halden"] = "High Halden",
 }
 
---- Airbases of Syria
+--- Airbases of the Syria map:
 --
 -- * AIRBASE.Syria.Kuweires
 -- * AIRBASE.Syria.Marj_Ruhayyil
@@ -348,7 +351,7 @@ AIRBASE.TheChannel = {
 -- * AIRBASE.Syria.Pinarbashi
 -- * AIRBASE.Syria.Paphos
 -- * AIRBASE.Syria.Kingsfield
--- * AIRBASE.Syria.Tha'lah
+-- * AIRBASE.Syria.Thalah
 -- * AIRBASE.Syria.Haifa
 -- * AIRBASE.Syria.Khalkhalah
 -- * AIRBASE.Syria.Megiddo
@@ -404,7 +407,7 @@ AIRBASE.Syria={
   ["Pinarbashi"]="Pinarbashi",
   ["Paphos"]="Paphos",
   ["Kingsfield"]="Kingsfield",
-  ["Tha'lah"]="Tha'lah",
+  ["Thalah"]="Tha'lah",
   ["Haifa"]="Haifa",
   ["Khalkhalah"]="Khalkhalah",
   ["Megiddo"]="Megiddo",
@@ -416,7 +419,6 @@ AIRBASE.Syria={
   ["Akrotiri"]="Akrotiri",
   ["Naqoura"]="Naqoura",
   ["Gaziantep"]="Gaziantep",
-  ["CVN_71"]="CVN-71",
   ["Sayqal"]="Sayqal",
   ["Tiyas"]="Tiyas",
   ["Shayrat"]="Shayrat",
@@ -441,22 +443,23 @@ AIRBASE.Syria={
 
 
 
---- Airbases of the Mariana Islands map.
+--- Airbases of the Mariana Islands map:
 --
--- * AIRBASE.MarianaIslands.Rota_International_Airport
--- * AIRBASE.MarianaIslands.Andersen
--- * AIRBASE.MarianaIslands.Northwest_Field
--- * AIRBASE.MarianaIslands.Antonio_B_Won_Pat_International_Airport
--- * AIRBASE.MarianaIslands.Saipan_International_Airport
--- * AIRBASE.MarianaIslands.Tinian_International_Airport
--- @field MarianaIslands
+-- * AIRBASE.MarianaIslands.Rota_Intl
+-- * AIRBASE.MarianaIslands.Andersen_AFB
+-- * AIRBASE.MarianaIslands.Antonio_B_Won_Pat_Intl
+-- * AIRBASE.MarianaIslands.Saipan_Intl
+-- * AIRBASE.MarianaIslands.Tinian_Intl
+-- * AIRBASE.MarianaIslands.Olf_Orote
+--
+--@field MarianaIslands
 AIRBASE.MarianaIslands={
-  ["Rota_International_Airport"]="Rota International Airport",
-  ["Andersen"]="Andersen",
-  ["Northwest_Field"]="Northwest_Field",
-  ["Antonio_B_Won_Pat_International_Airport"]="Antonio B. Won Pat International Airport",
-  ["Saipan_International_Airport"]="Saipan International Airport",
-  ["Tinian_International_Airport"]="Tinian International Airport",
+  ["Rota_Intl"]="Rota Intl",
+  ["Andersen_AFB"]="Andersen AFB",
+  ["Antonio_B_Won_Pat_Intl"]="Antonio B. Won Pat Intl",
+  ["Saipan_Intl"]="Saipan Intl",
+  ["Tinian_Intl"]="Tinian Intl",
+  ["Olf_Orote"]="Olf Orote",
 }
 
 
