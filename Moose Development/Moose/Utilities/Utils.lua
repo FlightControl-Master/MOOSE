2d4f90d5eb0811a2d039d7701674ef4909789f8e
Applevangelist <post@tniedermeier.com>
2022-01-23 11:37:07 +0100

    Added new Callsigns as per 2.7.9

--- Moose Development/Moose/Utilities/Utils.lua
+++ Moose Development/Moose/Utilities/Utils.lua
@@ -1,4 +1,4 @@
---- This module contains derived utilities taken from the MIST framework, which are excellent tools to be reused in an OO environment.
+--- This module contains derived utilities taken from the MIST framework, as well as a lot of added helpers from the MOOSE community.
 --
 -- ### Authors:
 --
@@ -7,6 +7,7 @@
 -- ### Contributions:
 --
 --   * FlightControl : Rework to OO framework.
+--   * And many more
 --
 -- @module Utils
 -- @image MOOSE.JPG
@@ -62,73 +63,129 @@ DCSMAP = {
 
 --- See [DCS_enum_callsigns](https://wiki.hoggitworld.com/view/DCS_enum_callsigns)
 -- @type CALLSIGN
-CALLSIGN = {
+CALLSIGN={
   -- Aircraft
-  Aircraft = {
-    Enfield = 1,
-    Springfield = 2,
-    Uzi = 3,
-    Colt = 4,
-    Dodge = 5,
-    Ford = 6,
-    Chevy = 7,
-    Pontiac = 8,
+  Aircraft={
+    Enfield=1,
+    Springfield=2,
+    Uzi=3,
+    Colt=4,
+    Dodge=5,
+    Ford=6,
+    Chevy=7,
+    Pontiac=8,
     -- A-10A or A-10C
-    Hawg = 9,
-    Boar = 10,
-    Pig = 11,
-    Tusk = 12,
+    Hawg=9,
+    Boar=10,
+    Pig=11,
+    Tusk=12,
   },
   -- AWACS
-  AWACS = {
-    Overlord = 1,
-    Magic = 2,
-    Wizard = 3,
-    Focus = 4,
-    Darkstar = 5,
+  AWACS={
+    Overlord=1,
+    Magic=2,
+    Wizard=3,
+    Focus=4,
+    Darkstar=5,
   },
   -- Tanker
-  Tanker = {
-    Texaco = 1,
-    Arco = 2,
-    Shell = 3,
+  Tanker={
+    Texaco=1,
+    Arco=2,
+    Shell=3,
   },
   -- JTAC
-  JTAC = {
-    Axeman = 1,
-    Darknight = 2,
-    Warrior = 3,
-    Pointer = 4,
-    Eyeball = 5,
-    Moonbeam = 6,
-    Whiplash = 7,
-    Finger = 8,
-    Pinpoint = 9,
-    Ferret = 10,
-    Shaba = 11,
-    Playboy = 12,
-    Hammer = 13,
-    Jaguar = 14,
-    Deathstar = 15,
-    Anvil = 16,
-    Firefly = 17,
-    Mantis = 18,
-    Badger = 19,
+  JTAC={
+    Axeman=1,
+    Darknight=2,
+    Warrior=3,
+    Pointer=4,
+    Eyeball=5,
+    Moonbeam=6,
+    Whiplash=7,
+    Finger=8,
+    Pinpoint=9,
+    Ferret=10,
+    Shaba=11,
+    Playboy=12,
+    Hammer=13,
+    Jaguar=14,
+    Deathstar=15,
+    Anvil=16,
+    Firefly=17,
+    Mantis=18,
+    Badger=19,
   },
   -- FARP
-  FARP = {
-    London = 1,
-    Dallas = 2,
-    Paris = 3,
-    Moscow = 4,
-    Berlin = 5,
-    Rome = 6,
-    Madrid = 7,
-    Warsaw = 8,
-    Dublin = 9,
-    Perth = 10,
+  FARP={
+    London=1,
+    Dallas=2,
+    Paris=3,
+    Moscow=4,
+    Berlin=5,
+    Rome=6,
+    Madrid=7,
+    Warsaw=8,
+    Dublin=9,
+    Perth=10,
   },
-} -- #CALLSIGN
+  F16={
+    Viper=9,
+    Venom=10,
+    Lobo=11,
+    Cowboy=12,
+    Python=13,
+    Rattler=14,
+    Panther=15,
+    Wolf=16,
+    Weasel=17,
+    Wild=18,
+    Ninja=19,
+    Jedi=20,
+  },
+  F18={
+    Hornet=9,
+    Squid=10,
+    Ragin=11,
+    Roman=12,
+    Sting=13,
+    Jury=14,
+    Jokey=15,
+    Ram=16,
+    Hawk=17,
+    Devil=18,
+    Check=19,
+    Snake=20,
+  },
+  F15E={
+    Dude=9,
+    Thud=10,
+    Gunny=11,
+    Trek=12,
+    Sniper=13,
+    Sled=14,
+    Best=15,
+    Jazz=16,
+    Rage=17,
+    Tahoe=18,
+  },
+  B1B={
+    Bone=9,
+    Dark=10,
+    Vader=11
+  },
+  B52={
+    Buff=9,
+    Dump=10,
+    Kenworth=11,
+  },
+  TransportAircraft={
+    Heavy=9,
+    Trash=10,
+    Cargo=11,
+    Ascot=12,
+  },
+} --#CALLSIGN
 
 --- Utilities static class.
 -- @type UTILS
