cad8f8678ed613d86723d50e2377d44980be7b72
Thomas <72444570+Applevangelist@users.noreply.github.com>
2023-01-23 10:53:58 +0100

    Adding ACLS

--- Moose Development/Moose/Wrapper/Controllable.lua
+++ Moose Development/Moose/Wrapper/Controllable.lua
@@ -668,6 +668,53 @@ function CONTROLLABLE:CommandActivateBeacon( Type, System, Frequency, UnitID, Ch
   return self
 end
 
+--- Activate ACLS system of the CONTROLLABLE. The controllable should be an aircraft carrier! Also needs Link4 to work.
+-- @param #CONTROLLABLE self
+-- @param #number UnitID The DCS UNIT ID of the unit the ACLS system is attached to. Useful if more units are in one group.
+-- @param #string Name (Optional) Name of the ACLS Beacon
+-- @param #number Delay (Optional) Delay in seconds before the ICLS is deactivated.
+-- @return #CONTROLLABLE self
+function CONTROLLABLE:CommandActivateACLS( UnitID, Name, Delay )
+
+  -- Command to activate ACLS system.
+  local CommandActivateACLS= { 
+  id = 'ActivateACLS', 
+  params = {
+    unitId = UnitID or self:GetID(), 
+    name = Name, 
+  } 
+}
+
+  if Delay and Delay > 0 then
+    SCHEDULER:New( nil, self.CommandActivateACLS, { self, UnitID, Name }, Delay )
+  else
+    self:SetCommand( CommandActivateACLS )
+  end
+
+  return self
+end
+
+--- Deactivate ACLS system of the CONTROLLABLE. The controllable should be an aircraft carrier!
+-- @param #CONTROLLABLE self
+-- @param #number Delay (Optional) Delay in seconds before the ICLS is deactivated.
+-- @return #CONTROLLABLE self
+function CONTROLLABLE:CommandDeactivateACLS( Delay )
+
+  -- Command to activate ACLS system.
+  local CommandDeactivateACLS= { 
+  id = 'DeactivateACLS', 
+  params = { } 
+}
+
+  if Delay and Delay > 0 then
+    SCHEDULER:New( nil, self.CommandDeactivateACLS, { self }, Delay )
+  else
+    self:SetCommand( CommandDeactivateACLS )
+  end
+
+  return self
+end
+
 --- Activate ICLS system of the CONTROLLABLE. The controllable should be an aircraft carrier!
 -- @param #CONTROLLABLE self
 -- @param #number Channel ICLS channel.
@@ -683,13 +730,13 @@ function CONTROLLABLE:CommandActivateICLS( Channel, UnitID, Callsign, Delay )
     params = {
       ["type"] = BEACON.Type.ICLS,
       ["channel"] = Channel,
-      ["unitId"] = UnitID,
+      ["unitId"] = UnitID or self:GetID(),
       ["callsign"] = Callsign,
     },
   }
 
   if Delay and Delay > 0 then
-    SCHEDULER:New( nil, self.CommandActivateICLS, { self }, Delay )
+    SCHEDULER:New( nil, self.CommandActivateICLS, { self, Channel, UnitID, Callsign }, Delay )
   else
     self:SetCommand( CommandActivateICLS )
   end
@@ -711,41 +758,13 @@ function CONTROLLABLE:CommandActivateLink4(Frequency, UnitID, Callsign, Delay)
     id = "ActivateLink4",
     params= {
       ["frequency "] = Frequency*1000,
-      ["unitId"] = UnitID,
+      ["unitId"] = UnitID or self:GetID(),
       ["name"] = Callsign,
     }
   }
 
   if Delay and Delay>0 then
-    SCHEDULER:New(nil, self.CommandActivateLink4, {self}, Delay)
-  else
-    self:SetCommand(CommandActivateLink4)
-  end
-
-  return self
-end
-
---- Activate LINK4 system of the CONTROLLABLE. The controllable should be an aircraft carrier!
--- @param #CONTROLLABLE self
--- @param #number Frequency Link4 Frequency in MHz, e.g. 336
--- @param #number UnitID The DCS UNIT ID of the unit the LINK4 system is attached to. Useful if more units are in one group.
--- @param #string Callsign Morse code identification callsign.
--- @param #number Delay (Optional) Delay in seconds before the LINK4 is deactivated.
--- @return #CONTROLLABLE self
-function CONTROLLABLE:CommandActivateLink4(Frequency, UnitID, Callsign, Delay)
-
-  -- Command to activate Link4 system.
-  local CommandActivateLink4= {
-    id = "ActivateLink4",
-    params= {
-      ["frequency "] = Frequency*1000,
-      ["unitId"] = UnitID,
-      ["name"] = Callsign,
-    }
-  }
-
-  if Delay and Delay>0 then
-    SCHEDULER:New(nil, self.CommandActivateLink4, {self}, Delay)
+    SCHEDULER:New(nil, self.CommandActivateLink4, {self, Frequency, UnitID, Callsign}, Delay)
   else
     self:SetCommand(CommandActivateLink4)
   end
@@ -804,24 +823,6 @@ function CONTROLLABLE:CommandDeactivateICLS( Delay )
     SCHEDULER:New( nil, self.CommandDeactivateICLS, { self }, Delay )
   else
     self:SetCommand( CommandDeactivateICLS )
-  end
-
-  return self
-end
-
---- Deactivate the active Link4 of the CONTROLLABLE.
--- @param #CONTROLLABLE self
--- @param #number Delay (Optional) Delay in seconds before the Link4 is deactivated.
--- @return #CONTROLLABLE self
-function CONTROLLABLE:CommandDeactivateLink4(Delay)
-
-  -- Command to deactivate
-  local CommandDeactivateLink4={id='DeactivateLink4', params={}}
-
-  if Delay and Delay>0 then
-    SCHEDULER:New(nil, self.CommandDeactivateLink4, {self}, Delay)
-  else
-    self:SetCommand(CommandDeactivateLink4)
   end
 
   return self
