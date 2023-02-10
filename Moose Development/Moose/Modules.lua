3d5470eb222b7a5fd3245b977f7f33e387d37381
Applevangelist <post@tniedermeier.com>
2023-02-10 11:00:14 +0100

    #NET * Initial release

--- Moose Development/Moose/Modules.lua
+++ Moose Development/Moose/Modules.lua
@@ -46,6 +46,7 @@ __Moose.Include( 'Scripts/Moose/Wrapper/Airbase.lua' )
 __Moose.Include( 'Scripts/Moose/Wrapper/Scenery.lua' )
 __Moose.Include( 'Scripts/Moose/Wrapper/Marker.lua' )
 __Moose.Include( 'Scripts/Moose/Wrapper/Weapon.lua' )
+__Moose.Include( 'Scripts/Moose/Wrapper/Net.lua' )
 
 __Moose.Include( 'Scripts/Moose/Cargo/Cargo.lua' )
 __Moose.Include( 'Scripts/Moose/Cargo/CargoUnit.lua' )
