6600f4d536129687023b8d49b0cdb4611b63b4f9
Thomas <72444570+Applevangelist@users.noreply.github.com>
2023-07-11 20:04:11 +0200

    Update Spot.lua
    Test relasing

--- Moose Development/Moose/Core/Spot.lua
+++ Moose Development/Moose/Core/Spot.lua
@@ -316,7 +316,7 @@ do
   -- @param Event
   -- @param To
   function SPOT:onafterLasing( From, Event, To )
-  
+    if self.lasing then
     if self.Target and self.Target:IsAlive() then
       self.SpotIR:setPoint( self.Target:GetPointVec3():AddY(1):AddY(math.random(-100,100)/100):AddX(math.random(-100,100)/100):GetVec3() )
       self.SpotLaser:setPoint( self.Target:GetPointVec3():AddY(1):GetVec3() )
@@ -334,7 +334,8 @@ do
     else
       self:F( { "Target is not alive", self.Target:IsAlive() } )
     end
-  
+    end
+    return self
   end
 
   --- @param #SPOT self
