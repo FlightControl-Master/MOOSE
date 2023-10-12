6ba62e3e041669fb3799f06384b667f87a846021
Applevangelist <post@tniedermeier.com>
2023-10-12 17:56:31 +0200

    #AUFTRAG * bear trap for Bryce using RECON Auftrags

--- Moose Development/Moose/Ops/Auftrag.lua
+++ Moose Development/Moose/Ops/Auftrag.lua
@@ -2230,7 +2230,13 @@ function AUFTRAG:NewRECON(ZoneSet, Speed, Altitude, Adinfinitum, Randomly, Forma
 
   mission:_TargetFromObject(ZoneSet)
   
-  mission.missionZoneSet = ZoneSet
+  if ZoneSet:IsInstanceOf("SET_ZONE") then
+    mission.missionZoneSet = ZoneSet
+  else
+   mission.missionZoneSet = SET_ZONE:New()
+   mission.missionZoneSet:AddZone(ZoneSet)
+  end
+  
   
   mission.missionTask=mission:GetMissionTaskforMissionType(AUFTRAG.Type.RECON)
 
@@ -6698,8 +6704,6 @@ function AUFTRAG.CheckMissionCapabilityAll(MissionTypes, Capabilities)
 
   return res
 end
-
-
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
