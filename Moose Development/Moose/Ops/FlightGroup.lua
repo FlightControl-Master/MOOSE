c76507040143073b36d14de4d01fb1d782d8b146
Applevangelist <post@tniedermeier.com>
2023-11-06 18:35:17 +0100

    #FLIGHTGROUP * Resolve a stalemate when all jobs are done but _CheckGroupDone isn't called any longer

--- Moose Development/Moose/Ops/FlightGroup.lua
+++ Moose Development/Moose/Ops/FlightGroup.lua
@@ -1256,20 +1256,20 @@ function FLIGHTGROUP:Status()
     
       -- Check damage.
     self:_CheckDamage()
- 
+    
+    -- Get current mission (if any).
+    local mission=self:GetMissionCurrent()
+    
      -- TODO: Check if group is waiting?
     if self:IsWaiting() then
       if self.Twaiting and self.dTwait then
         if timer.getAbsTime()>self.Twaiting+self.dTwait then
           --self.Twaiting=nil
           --self.dTwait=nil
-          --self:Cruise()
+          --self:_CheckGroupDone()
         end
       end
     end
-    
-    -- Get current mission (if any).
-    local mission=self:GetMissionCurrent()
     
     -- If mission, check if DCS task needs to be updated.
     if mission and mission.updateDCSTask then
@@ -1363,7 +1363,7 @@ function FLIGHTGROUP:Status()
 
   else
     -- Check damage.
-    self:_CheckDamage()      
+    self:_CheckDamage()   
   end
     
   ---
@@ -1617,9 +1617,15 @@ function FLIGHTGROUP:Status()
   ---
 
   self:_PrintTaskAndMissionStatus()
-  
-  -- Current mission.
+
+  -- All done?
+  -- Get current mission (if any).
   local mission=self:GetMissionCurrent()
+  if not mission then
+    self.Twaiting=nil
+    self.dTwait=nil
+    self:_CheckGroupDone()
+  end
 
 end
 
