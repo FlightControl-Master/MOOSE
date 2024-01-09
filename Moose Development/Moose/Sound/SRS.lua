9211770056a14ce48313dd76493cd4d810f7701f
Applevangelist <post@tniedermeier.com>
2024-01-09 08:58:22 +0100

    xx

--- Moose Development/Moose/Sound/SRS.lua
+++ Moose Development/Moose/Sound/SRS.lua
@@ -271,13 +271,42 @@ MSRS.Voices = {
     ["David"] = "Microsoft David Desktop", -- en-US
     ["Zira"] = "Microsoft Zira Desktop", -- en-US
     ["Hortense"] = "Microsoft Hortense Desktop", --fr-FR
+    ["de-DE-Hedda"] = "Microsoft Hedda Desktop", -- de-DE
+    ["en-GB-Hazel"] = "Microsoft Hazel Desktop", -- en-GB
+    ["en-US-David"] = "Microsoft David Desktop", -- en-US
+    ["en-US-Zira"] = "Microsoft Zira Desktop", -- en-US
+    ["fr-FR-Hortense"] = "Microsoft Hortense Desktop", --fr-FR
     },
-  MicrosoftGRPC = {
-    ["Hedda"] = "Hedda", -- de-DE
+  MicrosoftGRPC = { -- en-US/GB voices only as of Jan 2024
+    --["Hedda"] = "Hedda", -- de-DE
     ["Hazel"] = "Hazel", -- en-GB
+    ["George"] = "George", -- en-GB
+    ["Susan"] = "Susan", -- en-GB
     ["David"] = "David", -- en-US
     ["Zira"] = "Zira", -- en-US
-    ["Hortense"] = "Hortense", --fr-FR
+    ["Mark"] = "Mark", -- en-US
+    --[[
+    ["James"] = "James", --en-AU
+    ["Catherine"] = "Catherine", --en-AU
+    ["Richard"] = "Richard", --en-CA
+    ["Linda"] = "Linda", --en-CA
+    ["Ravi"] = "Ravi", --en-IN
+    ["Heera"] = "Heera", --en-IN
+    ["Sean"] = "Sean", --en-IR
+    ["en_GB_Hazel"] = "Hazel", -- en-GB
+    ["en_GB_George"] = "George", -- en-GB
+    ["en_GB_Susan"] = "Susan", -- en-GB
+    ["en_US_David"] = "David", -- en-US
+    ["en_US_Zira"] = "Zira", -- en-US
+    ["en_US_Mark"] = "Mark", -- en-US
+    ["en_AU_James"] = "James", --en-AU
+    ["en_AU_Catherine"] = "Catherine", --en-AU
+    ["en_CA_Richard"] = "Richard", --en-CA
+    ["en_CA_Linda"] = "Linda", --en-CA
+    ["en_IN_Ravi"] = "Ravi", --en-IN
+    ["en_IN_Heera"] = "Heera", --en-IN
+    ["en_IR_Sean"] = "Sean", --en-IR
+    --]]     
     },  
   Google = {
     Standard = {
