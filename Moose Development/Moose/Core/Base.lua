e8c75b87958da7358e7e74a2f480cd4b9d89f425
Applevangelist <post@tniedermeier.com>
2024-02-22 11:52:43 +0100

    SPAWN:InitSpeedMPS() etc

--- Moose Development/Moose/Core/Base.lua
+++ Moose Development/Moose/Core/Base.lua
@@ -1153,6 +1153,7 @@ function BASE:_Serialize(Arguments)
   text = string.gsub(text,"\n","")
   text = string.gsub(text,"%(%(","%(")
   text = string.gsub(text,"%)%)","%)")
+  text = string.gsub(text,"(%s+)","")
   return text
 end
 
