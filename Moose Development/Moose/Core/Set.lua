500fe37ac476b845dee3fdf33a8ac808eebd044d
Applevangelist <post@tniedermeier.com>
2024-07-28 12:42:34 +0200

    SET

--- Moose Development/Moose/Core/Set.lua
+++ Moose Development/Moose/Core/Set.lua
@@ -2107,16 +2107,19 @@ do
       
       if group and group:IsAlive() and (Coalitions==nil or UTILS.IsAnyInTable(Coalitions, group:GetCoalition())) then
       
-        local coord=group:GetCoord()
+        local coord=group:GetCoordinate()
         
-        -- Distance between ref. coordinate and group coordinate.
-        local d=UTILS.VecDist3D(Coordinate, coord)
-      
-        if d<dmin then
-          dmin=d
-          gmin=group
+        local d
+        
+        if coord ~= nil then
+          -- Distance between ref. coordinate and group coordinate.
+          d=UTILS.VecDist3D(Coordinate, coord)
+        
+          if d<dmin then
+            dmin=d
+            gmin=group
+          end
         end
-        
       end
     
     end
