a988e674908e74c94384daa395ab0ad345b641e1
shaji <shafik@blueblinkone.com>
2025-06-20 12:20:58 +0200

    [ADDED] New Kola Airbases

--- Moose Development/Moose/Wrapper/Airbase.lua
+++ Moose Development/Moose/Wrapper/Airbase.lua
@@ -828,6 +828,12 @@ AIRBASE.Kola = {
   ["Enontekio"] = "Enontekio",
   ["Evenes"] = "Evenes",
   ["Hosio"] = "Hosio",
+  ["Kilpyavr"] = "Kilpyavr",
+  ["Afrikanda"] = "Afrikanda",
+  ["Kalevala"] = "Kalevala",
+  ["Koshka_Yavr"] = "Koshka Yavr",
+  ["Poduzhemye"] = "Poduzhemye",
+  ["Luostari"] = "Luostari",
 }
 
 --- Airbases of the Afghanistan map
