5d1123e7df5a5578924c48a5dd93386739269191
Applevangelist <post@tniedermeier.com>
2025-11-16 16:54:04 +0100

    xx

--- Moose Development/Moose/Navigation/Radios.lua
+++ Moose Development/Moose/Navigation/Radios.lua
@@ -43,6 +43,7 @@
 -- 
 -- **Note** that try to avoid hard coding stuff in Moose since DCS is updated frequently and things change. Therefore, the main source of information is either a file `radio.lua` that can be
 -- found in the installation directory of DCS for each map or a table that the user needs to provide.
+-- **Note** your `MissionScripting` environment needs to be desanitized to read this data.
 -- 
 -- # Basic Setup
 -- 
